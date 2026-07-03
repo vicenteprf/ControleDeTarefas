7e2353c7c6633c9755353ad0e0f7b6ea6de97f8d
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-03 16:08:33 -0300

    fix: atualiza CORS para permitir domínio do frontend em produção

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -15,7 +15,7 @@ class App {
   private middlewares() {
     this.server.use(
       cors({
-        origin: "http://localhost:5173",
+        origin: "https://controle-de-tarefas-zivz-o75f3hnsu.vercel.app/",
       }),
     );
 
